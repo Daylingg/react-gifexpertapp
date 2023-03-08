17e964303309945d6d68ed9d76e4c375e47dc494
Daylin Garcia <daygarciag@gmail.com>
2023-03-08 12:10:26 -0600

    actualizacio

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -2,7 +2,7 @@
 
 export const getGifs=async(categori)=>{
 
-    const url=`http://api.giphy.com/v1/gifs/search?q=${encodeURI(categori)}&limit=10&key=PDZAIxD39r9o6VSaL87MPeLL1MgpO1yU`
+    const url=`https://api.giphy.com/v1/gifs/search?q=${encodeURI(categori)}&limit=10&key=PDZAIxD39r9o6VSaL87MPeLL1MgpO1yU`
     //${encodeURI(categori)} se usa para eliminar los espacios q pueda tener la categoria
     //"http://api.giphy.com/v1/gifs/search?q=Rick&limit=10&key=PDZAIxD39r9o6VSaL87MPeLL1MgpO1yU"
 
